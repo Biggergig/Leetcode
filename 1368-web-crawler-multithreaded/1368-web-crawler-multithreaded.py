635cd001f33694cd51e5c5f1d82e95c038c51f20
Anshuman Dash <56092891+Biggergig@users.noreply.github.com>
2025-02-26 00:45:57 -0800

    Time: 252 ms (45.12%), Space: 26.8 MB (24.78%) - LeetHub

--- 1368-web-crawler-multithreaded/1368-web-crawler-multithreaded.py
+++ 1368-web-crawler-multithreaded/1368-web-crawler-multithreaded.py
@@ -10,26 +10,34 @@
 #        """
 
 from multiprocessing.pool import ThreadPool
+from queue import Queue # thread safe
+import threading
+
 class Solution:
     def __init__(self):
-        self.to_process = set()
+        self.queue = Queue()
         self.res = set()
+        self.res_lock = threading.Lock()
     def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
-        self.res.add(startUrl)
-        self.to_process.add(startUrl)
+        with self.res_lock:
+            self.res.add(startUrl)
+        self.queue.put(startUrl)
     
         def getHost(url):return url.split('/')[2]
 
         host = getHost(startUrl)
 
         def helper(url):
-            self.to_process.remove(url)
             for u in htmlParser.getUrls(url):
-                if getHost(u) == host and u not in self.res:
-                    self.res.add(u)
-                    self.to_process.add(u)
+                with self.res_lock:
+                    if getHost(u) == host and u not in self.res:
+                        self.res.add(u)
+                        self.queue.put(u)
         
         p = ThreadPool(18)
-        while self.to_process:
-            pool_output = p.map(helper, list(self.to_process))
+        while not self.queue.empty():
+            urls = []
+            while not self.queue.empty():
+                urls.append(self.queue.get())
+            pool_output = p.map(helper, urls)
         return list(self.res)
