9a41625bfef16830f6bf72cd9c2c0fe322bc35f9
Anshuman Dash <56092891+Biggergig@users.noreply.github.com>
2025-02-26 02:04:16 -0800

    Time: 0 ms (100%), Space: 18.1 MB (5.96%) - LeetHub

--- 2566-number-of-unequal-triplets-in-array/2566-number-of-unequal-triplets-in-array.py
+++ 2566-number-of-unequal-triplets-in-array/2566-number-of-unequal-triplets-in-array.py
@@ -4,13 +4,21 @@ class Solution:
     def unequalTriplets(self, nums: List[int]) -> int:
         C = Counter(nums)
         freq = [*C.values()]
-        print(freq)
-        ans = 0
-        for i in range(len(freq)):
-            for j in range(i+1,len(freq)):
-                for k in range(j+1,len(freq)):
-                    ans += freq[i]*freq[j]*freq[k]
+        ans=l=0
+        r=len(nums)
+        # Since we're doing a nested loop and summing it up, instead just keep a 
+        # running total of values to left and right to multiply with
+        for f in freq:
+            r -= f
+            ans += l * f * r
+            l += f
         return ans
+
+        # for i in range(len(freq)):
+        #     for j in range(i+1,len(freq)):
+        #         for k in range(j+1,len(freq)):
+        #             ans += freq[i]*freq[j]*freq[k]
+        # return ans
         # ans = 0
         # for i in range(len(nums)):
         #     for j in range(i+1,len(nums)):
