327b57ebe4afd2ace711bad687343d6046c24761
Anshuman Dash <56092891+Biggergig@users.noreply.github.com>
2025-02-26 17:24:24 -0800

    Time: 644 ms (89.4%), Space: 19 MB (36.76%) - LeetHub

--- 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.py
+++ 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.py
@@ -13,11 +13,29 @@ class Solution:
                 if l > 2:  # This single line speeds up a ton of time??
                     best = max(best, l)
         return best if best >=3 else 0
-    
-    # Below is wrong, since it skips a potentially shorter but better sequence
 
-    # can we just keep track of deltas? - no because its not subarray but subsequence
+
+    def lenLongestFibSubseq(self, arr: List[int]) -> int:
+        # This is with DP - the idea being DP[x][x1] = len
+        # where x1 is a prev value and the length from that sequence
+        DP = defaultdict(lambda: defaultdict(int))
+        nums = set(arr)
+        seen = set()
+        for n in arr:
+            for x1 in seen:
+                if n+x1 in nums: # this line is necessary for time!
+                    DP[n+x1][n] = max(DP[n+x1][n], 2)
+            for x1, l in DP[n].items():
+                DP[n+x1][n] = max(DP[n+x1][n], l+1)
+            seen.add(n)
+        v = max([max(D.values()) for D in DP.values() if D]+[0])
+        return v if v>=3 else 0
+
+    
+    # Below is wrong, since it skips a potentially shorter but better sequence - needs to be 2d
+
     def WRONG_lenLongestFibSubseq(self, arr: List[int]) -> int:
+        # can we just keep track of deltas? - no because its not subarray but subsequence
         # key is the value we are looking for, values = [len, x_-1]
         tails = defaultdict(lambda: (-inf,inf)) # default to a value that is always smaller than any other tuple
         seen = set() # measure singletons to start sequences
