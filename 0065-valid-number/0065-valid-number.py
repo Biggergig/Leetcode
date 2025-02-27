33cb895d5e79f0b4cea977827bc699721811d687
Anshuman Dash <56092891+Biggergig@users.noreply.github.com>
2025-02-26 18:18:55 -0800

    Time: 4 ms (11.8%), Space: 17.9 MB (20.58%) - LeetHub

--- 0065-valid-number/0065-valid-number.py
+++ 0065-valid-number/0065-valid-number.py
@@ -2,3 +2,11 @@ import re
 class Solution:
     def isNumber(self, s: str) -> bool:
         return re.fullmatch(r"[+-]?((\d*\.?\d+)|(\d+\.?\d*))([eE][+-]?\d+)?",s) is not None
+
+    def isNumber(self, s: str) -> bool:
+        try:
+            if "n" in s: return False
+            float(s)
+            return True
+        except:
+            return False
