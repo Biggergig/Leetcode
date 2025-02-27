93e376218bd91fa79bab79dc27b86e4e339bfcc2
Anshuman Dash <56092891+Biggergig@users.noreply.github.com>
2025-02-26 18:54:30 -0800

    Time: 78 ms (69.5%), Space: 30.9 MB (27.86%) - LeetHub

--- 0894-random-pick-with-blacklist/0894-random-pick-with-blacklist.py
+++ 0894-random-pick-with-blacklist/0894-random-pick-with-blacklist.py
@@ -4,18 +4,18 @@ class Solution:
         self.mapping = {}
         self.size = n - len(blacklist)
         r=n-1
+        # move blacklist elements to end of array "swapping" them, then sample only from remaining
         blacklist = set(blacklist)
         for l in blacklist:
+            if l>=self.size: continue # if it's already in the end portion we can skip it
             while r in blacklist:
                 r-=1
-            if l>r: break
             self.mapping[l] = r
             r-=1
         print(self.mapping)
     def pick(self) -> int:
         i = randrange(self.size)
         return self.mapping.get(i,i)
-        
 
 
 # Your Solution object will be instantiated and called as such:
