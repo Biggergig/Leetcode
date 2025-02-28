cb99d2ae2f8c4f1247c793c267dd82cee5d24427
Anshuman Dash <56092891+Biggergig@users.noreply.github.com>
2025-02-27 21:02:24 -0800

    Time: 439 ms (58.23%), Space: 55.6 MB (30.92%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.py
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.py
@@ -13,3 +13,32 @@ class Solution:
                     nextRow[i1+1] = min(prevRow[i1+1]+c2, nextRow[i1]+c1, key=len)
             prevRow = nextRow
         return nextRow[-1]
+
+    # don't store string explicitly, but rather supersequence length
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        DP = [[inf]*(len(str1)+1) for _ in range(len(str2)+1)]
+        for y in range(len(DP)): DP[y][0] = y
+        for x in range(len(DP[0])): DP[0][x] = x
+
+        for y in range(1, len(DP)):
+            for x in range(1, len(DP[0])):
+                if str1[x-1] == str2[y-1]:
+                    DP[y][x] = DP[y-1][x-1]+1
+                else:
+                    DP[y][x] = min(DP[y-1][x],DP[y][x-1])+1
+                
+        out = []
+        x,y=len(DP[0])-1,len(DP)-1
+        while x or y:
+            print(x,y)
+            if x and DP[y][x-1] == DP[y][x]-1:
+                x-=1
+                out+=str1[x]
+            elif y and DP[y-1][x] == DP[y][x]-1:
+                y-=1
+                out+=str2[y]
+            else:
+                y-=1
+                x-=1
+                out+=str2[y]
+        return "".join(reversed(out))
