e764d05b0f5b8e35b2b83cd17844f2b15f941863
Anshuman Dash <56092891+Biggergig@users.noreply.github.com>
2025-02-28 01:44:10 -0800

    Time: 0 ms (100%), Space: 17.6 MB (89.79%) - LeetHub

--- 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.py
+++ 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.py
@@ -1,14 +1,12 @@
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
         pos = 0
-        d = -1j
-        for _ in range(4):
-            for i in instructions:
-                if i == "G":
-                    pos+=d
-                elif i == "L":
-                    d*=1j
-                elif i == "R":
-                    d*=-1j
-            if d == -1j:
-                return pos == 0
+        d = 1
+        for i in instructions:
+            if i == "G":
+                pos+=d
+            elif i == "L":
+                d*=1j
+            elif i == "R":
+                d*=-1j
+        return d!=1 or pos==0
