1c3c6490698145fae0335b267c5e7c806bccc157
Anshuman Dash <56092891+Biggergig@users.noreply.github.com>
2025-03-03 14:30:57 -0800

    Time: 43 ms (91.44%), Space: 28.5 MB (93.55%) - LeetHub

--- 1762-furthest-building-you-can-reach/1762-furthest-building-you-can-reach.py
+++ 1762-furthest-building-you-can-reach/1762-furthest-building-you-can-reach.py
@@ -1,17 +1,14 @@
 from heapq import *
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
-        deltas = [max(b-a,0) for a,b in zip(heights,heights[1:])]
         used_lad = []
-        for i,d in enumerate(deltas):
-            if d == 0: continue
-            if len(used_lad)==ladders and used_lad and d < used_lad[0]:
-                bricks -= d
-            else:
-                heappush(used_lad,d)
-                if len(used_lad) > ladders:
-                    need_bricks = heappop(used_lad)
-                    bricks -= need_bricks
-            if bricks < 0:
-                return i
+        for i in range(len(heights)-1):
+            d = heights[i+1]-heights[i]
+            if d <= 0: continue
+            heappush(used_lad,d)
+            if len(used_lad) > ladders:
+                need_bricks = heappop(used_lad)
+                if bricks < need_bricks:
+                    return i
+                bricks -= need_bricks
         return len(heights)-1
