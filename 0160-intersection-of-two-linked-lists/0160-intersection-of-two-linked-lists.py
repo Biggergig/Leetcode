7db3da0dddaca3ed5063e90687374b2aa4451d18
Anshuman Dash <56092891+Biggergig@users.noreply.github.com>
2025-03-02 22:52:40 -0800

    Time: 88 ms (42.43%), Space: 28.1 MB (25.51%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -12,17 +12,19 @@ class Solution:
                 ptr[i] = [headB,headA][i] if ptr[i] is None else ptr[i].next
         return ptr[0]
 
-        # length = [0,0]
-        # for i,head in enumerate([headA,headB]):
-        #     while head:
-        #         length[i]+=1
-        #         head = head.next
-        # for _ in range(max(length[0]-length[1], 0)):
-        #     headA = headA.next
-        # for _ in range(max(length[1]-length[0], 0)):
-        #     headB = headB.next
-        # while headA:
-        #     if headA == headB:
-        #         return headA
-        #     headA = headA.next
-        #     headB = headB.next
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        nodes = [headA, headB]
+        depths = [0,0]
+        for i in range(2):
+            while nodes[i] is not None:
+                nodes[i] = nodes[i].next
+                depths[i]+=1
+        nodes = [headA, headB]
+        for _ in range(depths[1],depths[0]):
+            nodes[0] = nodes[0].next
+        for _ in range(depths[0],depths[1]):
+            nodes[1] = nodes[1].next
+        while nodes[0] != nodes[1]:
+            nodes[0] = nodes[0].next
+            nodes[1] = nodes[1].next
+        return nodes[0]
