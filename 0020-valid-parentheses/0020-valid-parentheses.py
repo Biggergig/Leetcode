6c9129f73d183d98351e194ca679d53dcae91980
Anshuman Dash <56092891+Biggergig@users.noreply.github.com>
2025-03-03 14:12:20 -0800

    Time: 0 ms (100%), Space: 18 MB (32.43%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,15 +1,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        pairings = dict(zip(")}]","({["))
         S = []
-        mapping = dict(zip("})]", "{(["))
         for c in s:
-            if c in mapping:
-                if S == [] or S[-1] != mapping[c]:
-                    return False
+            if c in pairings:
+                if not S or S[-1] != pairings[c]: return False
                 S.pop()
             else:
                 S.append(c)
-        return S == []
-            
-
-            
+        return not S
