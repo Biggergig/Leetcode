ac0a360676a57822bcad90fbdc19e1b0d5a8b00e
Anshuman Dash <56092891+Biggergig@users.noreply.github.com>
2025-03-02 19:08:15 -0800

    Time: 0 ms (100%), Space: 17.7 MB (71.78%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -6,16 +6,16 @@ class Solution:
         m-=1
         n-=1
         for i in range(len(nums1)-1,-1,-1):
-            # print(nums1, nums2, m, n, i)
-            if n == -1:
-                nums1[i] = nums1[m]
-                m-=1
-            elif m == -1:
-                nums1[i] = nums2[n]
+            if n>=0 and m>=0:
+                if nums1[m]>nums2[n]:
+                    nums1[i]=nums1[m]
+                    m-=1
+                else:
+                    nums1[i]=nums2[n]
+                    n-=1
+            elif n>=0:
+                nums1[i]=nums2[n]
                 n-=1
-            elif nums1[m] > nums2[n]:
-                nums1[i] = nums1[m]
+            elif m>=0:
+                nums1[i]=nums1[m]
                 m-=1
-            else:
-                nums1[i] = nums2[n]
-                n-=1
