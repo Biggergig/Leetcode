b205a0468e1c7da8c6ae6b4f6919121879ae52a4
Anshuman Dash <56092891+Biggergig@users.noreply.github.com>
2025-03-03 14:29:47 -0800

    Time: 91 ms (10.61%), Space: 29.7 MB (6.45%) - LeetHub

--- 1762-furthest-building-you-can-reach/1762-furthest-building-you-can-reach.py
+++ 1762-furthest-building-you-can-reach/1762-furthest-building-you-can-reach.py
@@ -5,10 +5,13 @@ class Solution:
         used_lad = []
         for i,d in enumerate(deltas):
             if d == 0: continue
-            heappush(used_lad,d)
-            if len(used_lad) > ladders:
-                need_bricks = heappop(used_lad)
-                if bricks < need_bricks:
-                    return i
-                bricks -= need_bricks
+            if len(used_lad)==ladders and used_lad and d < used_lad[0]:
+                bricks -= d
+            else:
+                heappush(used_lad,d)
+                if len(used_lad) > ladders:
+                    need_bricks = heappop(used_lad)
+                    bricks -= need_bricks
+            if bricks < 0:
+                return i
         return len(heights)-1
