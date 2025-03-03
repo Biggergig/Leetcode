ff8dfc9a93c0f8f952d92975838b94745acefe7f
Anshuman Dash <56092891+Biggergig@users.noreply.github.com>
2025-03-02 23:01:41 -0800

    Time: 11 ms (83.57%), Space: 20.7 MB (63.73%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,7 @@
 from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        def hash(w):
-            out = [0]*26
-            for c in w:
-                out[ord(c)-ord('a')]+=1
-            return tuple(out)
         bins = defaultdict(list)
-        for w in strs:
-            bins[hash(w)].append(w)
+        for s in strs:
+            bins["".join(sorted(s))].append(s)
         return list(bins.values())
