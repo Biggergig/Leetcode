913ccd241749d421d6eb44bc4cb458d80be0ca31
Anshuman Dash <56092891+Biggergig@users.noreply.github.com>
2025-03-04 12:02:17 -0800

    Time: 248 ms (59.81%), Space: 20.1 MB (69.15%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,34 @@
+class Solution:
+    def DFS(self, x, y):
+        if not (0<=x<len(self.grid[0]) and 0<=y<len(self.grid)): return 0
+        if self.grid[y][x] != '1': return 0
+        self.grid[y][x] = '0'
+        self.DFS(x+1,y)
+        self.DFS(x-1,y)
+        self.DFS(x,y+1)
+        self.DFS(x,y-1)
+        return 1
+    def numIslands(self, grid: List[List[str]]) -> int:
+        self.grid = grid
+        ans = 0
+        for y in range(len(grid)):
+            for x in range(len(grid[0])):
+                ans += self.DFS(x,y)
+
+        return ans
+    
+    def numIslands(self, grid: List[List[str]]) -> int:
+        ans = 0
+        def mark(x,y):
+            if not (0<=x<len(grid[0]) and 0<=y<len(grid)) or grid[y][x] == '0': return 0
+            grid[y][x] = '0'
+            mark(x-1,y)
+            mark(x+1,y)
+            mark(x,y-1)
+            mark(x,y+1)
+            return 1
+        for y in range(len(grid)):
+            for x in range(len(grid[0])):
+                if grid[y][x] == '1':
+                    ans+=mark(x,y)
+        return ans
