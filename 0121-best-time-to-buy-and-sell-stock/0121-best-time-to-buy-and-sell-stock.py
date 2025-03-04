63649a169b84d92805b48ec5eb53849f466e3963
Anshuman Dash <56092891+Biggergig@users.noreply.github.com>
2025-03-04 11:30:17 -0800

    Time: 31 ms (87.1%), Space: 27 MB (33.35%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,5 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        mn,best = inf,0
+        for n in prices:
+            best = max(best,n-mn)
+            mn=min(mn,n)
+        return best
+    
+    # Faster than using max/min
+    def maxProfit(self, prices: List[int]) -> int:
         mn = inf
         best = 0
         for n in prices:
